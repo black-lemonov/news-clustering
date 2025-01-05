6bef25c8cb1005540e6d255b1d6cc35ca1c6217a
black-lemonov <egor.porf1@mail.ru>
2025-01-06 00:58:49 +0300

    классы для парсеров

--- parsing/parsers.py
+++ parsing/parsers.py
@@ -6,8 +6,8 @@ from datetime import datetime, timedelta
 import httpx
 from scrapy import Selector
 
-from parsing.logger import Logger
-from parsing.repository import NewsRepository, News
+from logger import Logger
+from repository import NewsRepository, News
 
 
 class Parser(ABC):
@@ -21,6 +21,8 @@ class Parser(ABC):
     _parsed_urls_buffer_limit: int = 30
     _parsed_urls_buffer: deque[str]   # здесь будет очередь из разных новостей
     _tmp_urls_buffer: deque[str]    # здесь будут все новости с одной страницы
+
+    _stopwords: set[str] = {"ТОП", "Топ", "топ"}
 
     _article: Selector
     _parsed_news: News
@@ -36,13 +38,14 @@ class Parser(ABC):
             repositories: list[NewsRepository],
             logger: Logger,
             http_client: httpx.AsyncClient,
-            parse_interval: int
+            parse_interval_sec: int
     ) -> None:
         self._repositories = repositories
         self._logger = logger
         self._http_client = http_client
-        self._parse_interval_sec = parse_interval
+        self._parse_interval_sec = parse_interval_sec
         self._parsed_urls_buffer = deque(maxlen=self._parsed_urls_buffer_limit)
+        self._tmp_urls_buffer = deque()
 
     def add_storage(self, storage: NewsRepository) -> None:
         self._repositories.append(storage)
@@ -60,6 +63,8 @@ class Parser(ABC):
                 continue
             self._save_to_tmp_buffer()
             self._parse_title()
+            if self._is_spam():
+                continue
             self._parse_date()
             await self._sleep()
             await self._parse_content()
@@ -99,6 +104,10 @@ class Parser(ABC):
     def _parse_title(self) -> None:
         title = self._article.css(self._title_css).get().strip()
         self._parsed_news.title = title
+
+    def _is_spam(self) -> bool:
+        title = self._parsed_news.title
+        return title in self._stopwords
 
     def _parse_date(self) -> None:
         date = self._article.css(self._date_css).get().strip()
