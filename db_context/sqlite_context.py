27cc704fa147e4e55369e47a3ac558583bb1f970
black-lemonov <egor.porf1@mail.ru>
2025-01-07 01:29:34 +0300

    рефакторинг, добавлена многопоточность

--- db_context/sqlite_context.py
+++ db_context/sqlite_context.py
@@ -1,14 +1,18 @@
 import sqlite3
+from sqlite3 import Connection
 
 from db_context.abc_context import DBContext
 from dto.article import Article
 
 
 class SQLiteDBContext(DBContext):
-    __db_path: str
+    __con: Connection
 
     def __init__(self, db_path: str):
-        self.__db_path = db_path
+        self.__con = sqlite3.connect(
+            database=db_path,
+            check_same_thread=False
+        )
 
     @staticmethod
     def __article_factory(cursor, row):
@@ -20,44 +24,41 @@ class SQLiteDBContext(DBContext):
         return row[0]
 
     def insert(self, values: tuple[str, str, str, str]) -> None:
-        with sqlite3.Connection(self.__db_path) as con:
-            con.execute(
+        with self.__con:
+            self.__con.execute(
                 "INSERT INTO Articles (url, title, description, date) VALUES(?, ?, ?, ?)",
                 values
             )
 
     def delete_where_days_limit(self, days_limit: int) -> None:
-        with sqlite3.Connection(self.__db_path) as con:
-            con.execute(
+        with self.__con:
+            self.__con.execute(
                 "DELETE FROM Articles WHERE julianday(date('now')) - julianday(date) > ?",
                 (days_limit,)
             )
 
     def select_min_date_by_clusters(self) -> list[Article]:
-        with sqlite3.Connection(self.__db_path) as con:
-            con.row_factory = self.__article_factory
-            return con.execute(
-                "SELECT cluster_n, title, min(date) as date FROM Articles GROUP BY cluster_n"
-            ).fetchall()
+        self.__con.row_factory = self.__article_factory
+        return self.__con.execute(
+            "SELECT cluster_n, title, min(date) as date FROM Articles GROUP BY cluster_n"
+        ).fetchall()
 
     def select_by_cluster(self, cluster_n: int) -> list[Article]:
-        with sqlite3.Connection(self.__db_path) as con:
-            con.row_factory = self.__article_factory
-            return con.execute(
-                "SELECT url, title, date FROM Articles WHERE cluster_n = ?",
-                (cluster_n,)
-            ).fetchall()
+        self.__con.row_factory = self.__article_factory
+        return self.__con.execute(
+            "SELECT url, title, date FROM Articles WHERE cluster_n = ?",
+            (cluster_n,)
+        ).fetchall()
 
     def select_descriptions(self) -> list[str]:
-        with sqlite3.connect(self.__db_path) as con:
-            con.row_factory = self.__single_field_factory
-            return con.execute(
-                "SELECT description FROM Articles"
-            ).fetchall()
+        self.__con.row_factory = self.__single_field_factory
+        return self.__con.execute(
+            "SELECT description FROM Articles"
+        ).fetchall()
 
     def set_cluster_n_where_description(self, clusters: list[int], descriptions: list[str]) -> None:
-        with sqlite3.connect(self.__db_path) as con:
-            con.executemany(
+        with self.__con:
+            self.__con.executemany(
                 "UPDATE Articles SET cluster_n = ? WHERE description = ?",
                 [
                     (int(cluster_n), description)
@@ -65,3 +66,6 @@ class SQLiteDBContext(DBContext):
                     in zip(descriptions, clusters)
                 ]
             )
+
+    def __del__(self):
+        self.__con.close()
